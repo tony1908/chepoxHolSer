8786f8deb5e667ff5b839739369cfca9b23b7216
Antonio <toony1908@gmail.com>
2016-05-08 10:38:12 -0500

    segunda ruta de mensajes

--- index.js
+++ index.js
@@ -8,6 +8,18 @@ var client = new havenondemand.HODClient("4eacd446-c06d-4f37-8724-e3a7e8d7c434")
 
 var app = express();
 
+// var myJson={"players":[
+//             {"name":"Messi", "goals":8},            
+//             {"name":"Ronaldo", "goals":22},
+//             {"name":"Costa", "goals":20},
+//             {"name":"Neymar", "goals":13},
+//             {"name":"Arabi", "goals":6},
+//             {"name":"Bale", "goals":3},
+//             {"name":"Toquero", "goals":0}]};
+
+var myJson = []
+
+
 app.get('/', function (req, res) {
   res.json({ version: packageInfo.version });
 });
@@ -20,11 +32,17 @@ app.get('/api', function (req, res) {
 	  var len = (resp.body.language_iso639_2b != "UND") ? resp.body.language_iso639_2b : "SPA"
 	  var dat = {'text' : query, "language" : len}
 	  client.call('analyzesentiment', dat, function(err, resp, body) {
+	  	  myJson.push({ sent: resp.body.aggregate.sentiment, lang: len });
 		  res.json({ sent: resp.body.aggregate.sentiment, lang: len });
 		})
 	})
 })
 
+app.get('/mens', function (req, res) {
+	console.log("entre")
+	res.contentType('application/json');
+	res.send(JSON.stringify(myJson));
+})
 
 
 var server = app.listen(process.env.PORT, function () {
