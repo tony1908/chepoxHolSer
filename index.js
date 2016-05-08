c8c69655699612851f47952ee7fe7abc1c960708
Antonio <toony1908@gmail.com>
2016-05-08 10:26:36 -0500

    llamdas a hpe

--- index.js
+++ index.js
@@ -3,12 +3,29 @@ var request = require('request')
 var bodyParser = require('body-parser')
 var jsonParser = bodyParser.json();
 var packageInfo = require('./package.json');
+var havenondemand = require('havenondemand');
+var client = new havenondemand.HODClient("4eacd446-c06d-4f37-8724-e3a7e8d7c434")
 
 var app = express();
 
 app.get('/', function (req, res) {
   res.json({ version: packageInfo.version });
 });
+
+
+app.get('/api', function (req, res) {
+	var query = req.query.texto
+	var data = {'text' : query}
+	client.call('identifylanguage', data, function(err, resp, body) {
+	  var len = (resp.body.language_iso639_2b != "UND") ? resp.body.language_iso639_2b : "SPA"
+	  var dat = {'text' : query, "language" : len}
+	  client.call('analyzesentiment', dat, function(err, resp, body) {
+		  res.json({ sent: resp.body.aggregate.sentiment, lang: len });
+		})
+	})
+})
+
+
 
 var server = app.listen(process.env.PORT, function () {
 	var host = server.address().address;
